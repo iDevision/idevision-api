daabc9e3489f6063a4d0271f5457a580e860d522
IAmTomahawkx <amanca@telus.net>
2020-04-14 22:53:09 -0700

    initial commit

--- launcher.py
+++ launcher.py
@@ -7,12 +7,19 @@ choices = list("qwertyuiopadfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890")
 app = web.Application()
 router = web.RouteTableDef()
 
+with open("authorization.txt") as f:
+    auth = f.read()
+
 @router.post("/media")
 async def post_media(request: web.Request):
+    authorization = request.headers.get("Authorization")
+    if authorization != auth:
+        return web.Response(text="403 Unauthorized", status=403)
+
     data = await request.post()
     file = data['upload']
     extension = file.filename.split(".").pop()
-    new_name = "".join(x for x in [random.choice(choices) for i in range(3)]) + f".{extension}"
+    new_name = "".join([random.choice(choices) for i in range(3)]) + f".{extension}"
     buffer = io.FileIO(f"media/{new_name}", mode="w")
     buffer.write(file.file.read())
     buffer.close()
