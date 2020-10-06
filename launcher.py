cfa1784c91c71cf49dc9d95b74678352968001a6
IAmTomahawkx <iamtomahawkx@gmail.com>
2020-10-06 14:32:41 -0700

    change auto-pull stuff

--- launcher.py
+++ launcher.py
@@ -418,7 +418,7 @@ async def git_checks(request: web.Request):
 
     if data['action'] == "completed" and data['check_run']['conclusion'] == "success":
         import subprocess
-        subprocess.run(["/usr/bin/bash", "-c", "at now"], input=b"git pull origin master && systemctl restart idevision")
+        subprocess.run(["/usr/bin/bash", "-c", "at now"], input=b"git pull origin master && systemctl --user restart idevision")
         print("restarting....")
 
     return web.Response()
